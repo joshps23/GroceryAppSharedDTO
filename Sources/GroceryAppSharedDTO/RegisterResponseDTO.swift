a4dee46688b4cb86b01fbda7cfe366333ad1a030
Joshua Goh <joshuagoh@Jawsures-MacBook-PRO.local>
2023-08-10 22:34:02 +0800

    register and login response

---
+++ Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joshua Goh on 10/8/23.
+//
+
+import Foundation
+
+public struct RegisterResponseDTO: Codable {
+    
+    public let error: Bool
+    public var reason: String? = nil
+    
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+    
+}
